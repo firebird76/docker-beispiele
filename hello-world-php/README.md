e622ba33ec6ceefec1f13bfd914f9fc88bb04899
Tobias Scharlewsky <tobias@scharlewsky.de>
2020-04-17 10:50:49 +0200

    Beispiel 1 hello-world-php

---
+++ hello-world-php/README.md
@@ -0,0 +1,2 @@
+#Apache mit PHP 7
+Ein einfacher PHP 7 Server mit index.php 
---
+++ hello-world-php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- Datei index.php -->
+<html>
+<head>
+  <title>Hello world</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+<h1>Hello world: apache/php</h1>
+<?php
+  $load = sys_getloadavg();
+?>
+  Serverzeit: <?php echo date("c"); ?><br />
+  Serverauslastung (load): <?php echo $load[0]; ?>
+</body>
+</html>
